1fe62d82a8e90db1b3849d37b6fe351833bc1558
Gé Vissers <ge@octonion.nl>
2015-04-24 15:00:59 +0200

    Add generic stack implementation

---
+++ Stack.js
@@ -0,0 +1,37 @@
+/**
+ * General purpose stack
+ *
+ * Stack using clone_fun to clone object to be pushed on the stack.
+ */
+function Stack(clone_fun)
+{
+	/// Clone function, used when pushing a new object on the stack
+	this.clone_fun = clone_fun;
+	/// The stack itself
+	this.stack = [];
+
+	/**
+	 * Push an object on the stack
+	 *
+	 * Push a clone of \a obj on the stack.
+	 * @param obj The object to push on the stack
+	 */
+	this.push = function(obj)
+	{
+		this.stack.push(this.clone_fun(obj));
+	}
+
+	/**
+	 * Pop from the stack
+	 *
+	 * Pop the last pushed object from the stack and return it. If the stack is
+	 * empty, throw an error.
+	 * @return The last object pushed on the stack
+	 */
+	this.pop = function()
+	{
+		if (this.stack.length == 0)
+			throw "Unable to pop empty stack";
+		return this.stack.pop();
+	}
+}
