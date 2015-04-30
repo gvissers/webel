4425988547d85b084d7442282273c0c2efb93b78
Gé Vissers <ge@octonion.nl>
2015-04-30 10:34:49 +0200

    Actually import the camera class we use

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 
 		<script type="text/javascript" src="ajaxblob.js"></script>
 
+		<script type="text/javascript" src="Camera.js"></script>
 		<script type="text/javascript" src="DDS.js"></script>
 		<script type="text/javascript" src="Stack.js"></script>
 		<script type="text/javascript" src="Shaders.js"></script>
