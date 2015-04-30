d3dc9ef109a26c66c2f059704a4b211669a48d75
Gé Vissers <ge@octonion.nl>
2015-04-30 11:42:32 +0200

    Move "game" scene into a separate class

--- drawscene.js
+++ drawscene.js
@@ -1,18 +1,14 @@
 var gl;
 var mvMatrix = mat4.create();
 var pMatrix = mat4.create();
-var pyramidVertexPositionBuffer;
-var pyramidVertexColorBuffer;
-var cubeVertexPositionBuffer;
-var cubeVertexTextureCoordBuffer;
-var cubeVertexIndexBuffer;
+var game_window;
 var lastTime = 0;
-var rPyramid = 0;
-var rCube = 0;
 var mvMatrixStack = new Stack(mat4.clone);
 var shaders = new Shaders();
 var texture_cache;
 camera = new Camera();
+var keys_pressed = {}
+
 
 function logError(msg)
 {
@@ -41,171 +37,6 @@ function setMatrixUniforms()
 	gl.uniformMatrix4fv(shaders.program.mvMatrixUniform, false, mvMatrix);
 }
 
-function initBuffers()
-{
-	pyramidVertexPositionBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, pyramidVertexPositionBuffer);
-	var vertices = [
-		// Front face
-		 0.0,  1.0,  0.0,
-		-1.0, -1.0,  1.0,
-		 1.0, -1.0,  1.0,
-
-		// Right face
-		 0.0,  1.0,  0.0,
-		 1.0, -1.0,  1.0,
-		 1.0, -1.0, -1.0,
-
-		// Back face
-		 0.0,  1.0,  0.0,
-		 1.0, -1.0, -1.0,
-		-1.0, -1.0, -1.0,
-
-		// Left face
-		 0.0,  1.0,  0.0,
-		-1.0, -1.0, -1.0,
-		-1.0, -1.0,  1.0
-	];
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-	pyramidVertexPositionBuffer.itemSize = 3;
-	pyramidVertexPositionBuffer.numItems = vertices.length / pyramidVertexPositionBuffer.itemSize;
-
-	pyramidVertexColorBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, pyramidVertexColorBuffer);
-	var colors = [
-		// Front face
-		1.0, 0.0, 0.0, 1.0,
-		0.0, 1.0, 0.0, 1.0,
-		0.0, 0.0, 1.0, 1.0,
-
-		// Right face
-		1.0, 0.0, 0.0, 1.0,
-		0.0, 0.0, 1.0, 1.0,
-		0.0, 1.0, 0.0, 1.0,
-
-		// Back face
-		1.0, 0.0, 0.0, 1.0,
-		0.0, 1.0, 0.0, 1.0,
-		0.0, 0.0, 1.0, 1.0,
-
-		// Left face
-		1.0, 0.0, 0.0, 1.0,
-		0.0, 0.0, 1.0, 1.0,
-		0.0, 1.0, 0.0, 1.0
-	];
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
-	pyramidVertexColorBuffer.itemSize = 4;
-	pyramidVertexColorBuffer.numItems = colors.length / pyramidVertexColorBuffer.itemSize;
-
-	cubeVertexPositionBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexPositionBuffer);
-	vertices = [
-		// Front face
-		-1.0, -1.0,  1.0,
-		 1.0, -1.0,  1.0,
-		 1.0,  1.0,  1.0,
-		-1.0,  1.0,  1.0,
-
-		// Back face
-		-1.0, -1.0, -1.0,
-		-1.0,  1.0, -1.0,
-		 1.0,  1.0, -1.0,
-		 1.0, -1.0, -1.0,
-
-		// Top face
-		-1.0,  1.0, -1.0,
-		-1.0,  1.0,  1.0,
-		 1.0,  1.0,  1.0,
-		 1.0,  1.0, -1.0,
-
-		// Bottom face
-		-1.0, -1.0, -1.0,
-		 1.0, -1.0, -1.0,
-		 1.0, -1.0,  1.0,
-		-1.0, -1.0,  1.0,
-
-		// Right face
-		 1.0, -1.0, -1.0,
-		 1.0,  1.0, -1.0,
-		 1.0,  1.0,  1.0,
-		 1.0, -1.0,  1.0,
-
-		// Left face
-		-1.0, -1.0, -1.0,
-		-1.0, -1.0,  1.0,
-		-1.0,  1.0,  1.0,
-		-1.0,  1.0, -1.0
-	];
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-	cubeVertexPositionBuffer.itemSize = 3;
-	cubeVertexPositionBuffer.numItems = vertices.length / cubeVertexPositionBuffer.itemSize;
-
-	cubeVertexTextureCoordBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexTextureCoordBuffer);
-	var textureCoords = [
-	// Front face
-	0.0, 0.0,
-	1.0, 0.0,
-	1.0, 1.0,
-	0.0, 1.0,
-
-	// Back face
-	1.0, 0.0,
-	1.0, 1.0,
-	0.0, 1.0,
-	0.0, 0.0,
-
-	// Top face
-	0.0, 1.0,
-	0.0, 0.0,
-	1.0, 0.0,
-	1.0, 1.0,
-
-	// Bottom face
-	1.0, 1.0,
-	0.0, 1.0,
-	0.0, 0.0,
-	1.0, 0.0,
-
-	// Right face
-	1.0, 0.0,
-	1.0, 1.0,
-	0.0, 1.0,
-	0.0, 0.0,
-
-	// Left face
-	0.0, 0.0,
-	1.0, 0.0,
-	1.0, 1.0,
-	0.0, 1.0,
-	];
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoords), gl.STATIC_DRAW);
-	cubeVertexTextureCoordBuffer.itemSize = 2;
-	cubeVertexTextureCoordBuffer.numItems = textureCoords.length / cubeVertexTextureCoordBuffer.itemSize;
-
-	cubeVertexIndexBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVertexIndexBuffer);
-	var cubeVertexIndices = [
-		0, 1, 2,      0, 2, 3,    // Front face
-		4, 5, 6,      4, 6, 7,    // Back face
-		8, 9, 10,     8, 10, 11,  // Top face
-		12, 13, 14,   12, 14, 15, // Bottom face
-		16, 17, 18,   16, 18, 19, // Right face
-		20, 21, 22,   20, 22, 23  // Left face
-	];
-	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeVertexIndices), gl.STATIC_DRAW);
-	cubeVertexIndexBuffer.itemSize = 1;
-	cubeVertexIndexBuffer.numItems = cubeVertexIndices.length / cubeVertexIndexBuffer.itemSize;
-}
-
-var xRot = 0;
-var xSpeed = 0;
-
-var yRot = 0;
-var ySpeed = 0;
-
-var filter = 0;
-
 function drawScene()
 {
 	if (!shaders.ready)
@@ -222,65 +53,13 @@ function drawScene()
 	mat4.identity(mvMatrix);
 	mat4.translate(mvMatrix, mvMatrix, camera.position);
 
-	mvMatrixStack.push(mvMatrix);
-	mat4.translate(mvMatrix, mvMatrix, [-1.5, 0.0, 0.0]);
-	mat4.rotate(mvMatrix, mvMatrix, rPyramid, [1, 1, 0]);
-	// Set vertex positions
-	gl.bindBuffer(gl.ARRAY_BUFFER, pyramidVertexPositionBuffer);
-	gl.vertexAttribPointer(shaders.program.vertexPositionAttribute,
-		pyramidVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
-	// Pyramid is drawn using solid colors, use single-pixel white texture
-	// for texture interpolation
-	texture_cache.bind("white");
-	// turn off texture coordinates.
-	gl.enableVertexAttribArray(shaders.program.vertexColorAttribute);
-	gl.disableVertexAttribArray(shaders.program.textureCoordAttribute);
-	gl.bindBuffer(gl.ARRAY_BUFFER, pyramidVertexColorBuffer);
-	gl.vertexAttribPointer(shaders.program.vertexColorAttribute,
-		pyramidVertexColorBuffer.itemSize, gl.FLOAT, false, 0, 0);
-	setMatrixUniforms();
-	gl.drawArrays(gl.TRIANGLES, 0, pyramidVertexPositionBuffer.numItems);
-	mvMatrix = mvMatrixStack.pop();
-
-	mvMatrixStack.push(mvMatrix);
-	mat4.translate(mvMatrix, mvMatrix, [1.5, 0.0, 0.0]);
-	mat4.rotate(mvMatrix, mvMatrix, xRot, [1, 0, 0]);
-	mat4.rotate(mvMatrix, mvMatrix, yRot, [0, 1, 0]);
-	// Set vertex positions
-	gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexPositionBuffer);
-	gl.vertexAttribPointer(shaders.program.vertexPositionAttribute,
-		cubeVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
-	// Cube uses texture only, disable color and use white
-	gl.disableVertexAttribArray(shaders.program.vertexColorAttribute);
-	gl.vertexAttrib4f(shaders.program.vertexColorAttribute, 1, 1, 1, 1);
-	// Set texture buffer
-	gl.enableVertexAttribArray(shaders.program.textureCoordAttribute);
-	gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexTextureCoordBuffer);
-	gl.vertexAttribPointer(shaders.program.textureCoordAttribute,
-		cubeVertexTextureCoordBuffer.itemSize, gl.FLOAT, false, 0, 0);
-	gl.activeTexture(gl.TEXTURE0);
-	texture_cache.bind("textures/items1.dds");
-	gl.uniform1i(shaders.program.samplerUniform, 0);
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVertexIndexBuffer);
-	setMatrixUniforms();
-	gl.drawElements(gl.TRIANGLES, cubeVertexIndexBuffer.numItems,
-		gl.UNSIGNED_SHORT, 0);
-	mvMatrix = mvMatrixStack.pop();
+	game_window.draw();
 }
-
 
 function animate()
 {
 	var timeNow = new Date().getTime();
-	if (lastTime != 0)
-	{
-		var elapsed = timeNow - lastTime;
-
-		rPyramid += (0.5 * Math.PI * elapsed) / 1000.0;
-		xRot -= (xSpeed * Math.PI/180 * elapsed) / 1000.0;
-		yRot -= (ySpeed * Math.PI/180 * elapsed) / 1000.0;
-	}
-	lastTime = timeNow;
+	game_window.animate(timeNow);
 }
 
 function tick()
@@ -291,44 +70,27 @@ function tick()
 	animate();
 }
 
-var pressedKeys = {}
-
 function handleKeyDown(event)
 {
-	pressedKeys[event.keyCode] = true;
+	keys_pressed[event.keyCode] = true;
 }
 
 function handleKeyUp(event)
 {
-	pressedKeys[event.keyCode] = false;
+	keys_pressed[event.keyCode] = false;
 }
 
 function handleKeys()
 {
-	if (pressedKeys[33]) {
+	if (keys_pressed[33]) {
       // Page Up
       camera.zoomOut();
     }
-    if (pressedKeys[34]) {
+    if (keys_pressed[34]) {
       // Page Down
       camera.zoomIn();
     }
-    if (pressedKeys[37]) {
-      // Left cursor key
-      ySpeed -= 1;
-    }
-    if (pressedKeys[39]) {
-      // Right cursor key
-      ySpeed += 1;
-    }
-    if (pressedKeys[38]) {
-      // Up cursor key
-      xSpeed -= 1;
-    }
-    if (pressedKeys[40]) {
-      // Down cursor key
-      xSpeed += 1;
-    }
+    game_window.handleKeys(keys_pressed);
 }
 
 function webGLStart()
@@ -337,8 +99,8 @@ function webGLStart()
 	initGL(canvas);
 
 	shaders.init();
-	initBuffers();
 	texture_cache = new TextureCache();
+	game_window = new GameWindow();
 
 	gl.clearColor(0.0, 0.0, 0.0, 1.0);
 	gl.enable(gl.DEPTH_TEST);
