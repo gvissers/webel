042312c6e0784a0a19f4ea887d2103c60bfe71ee
Gé Vissers <ge@octonion.nl>
2015-05-01 14:52:47 +0200

    Since we're using webgl-utils anyway, use it to create our GL context as well

--- drawscene.js
+++ drawscene.js
@@ -18,7 +18,7 @@ function initGL(canvas)
 {
 	try
 	{
-		gl = canvas.getContext("experimental-webgl");
+		gl = WebGLUtils.setupWebGL(canvas);
 		gl.viewportWidth = canvas.width;
 		gl.viewportHeight = canvas.height;
 	}
