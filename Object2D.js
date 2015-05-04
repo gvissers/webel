17f7c927aa763a5d98c90ec81367c9d4c2d2c3da
Gé Vissers <ge@octonion.nl>
2015-05-04 15:37:26 +0200

    Calculate transformed coordinates explicitly for 2d objects, instead of through model view matrix

--- Object2D.js
+++ Object2D.js
@@ -9,8 +9,6 @@ function Object2D(fname, pos, rot)
 	this.rotation = rot;
 	/// Object definition
 	this.def = null;
-	/// The transformation matrix for this object
-	this.matrix = null;
 	/// Vertices for this object
 	this.vertex_buffer = null;
 	/// Texture coordinates for this object
@@ -35,19 +33,18 @@ Object2D.prototype.setDefinition = function(def)
 		this.rot[0] += 90;
 	}
 
-	this.matrix = mat4.create();
-	mat4.identity(this.matrix);
-	mat4.translate(this.matrix, this.matrix, this.position);
-	mat4.rotateZ(this.matrix, this.matrix, this.rotation[2]*Math.PI/180);
-	mat4.rotateX(this.matrix, this.matrix, this.rotation[0]*Math.PI/180);
-	mat4.rotateY(this.matrix, this.matrix, this.rotation[1]*Math.PI/180);
+	var transform = mat4.create();
+	mat4.identity(transform);
+	mat4.translate(transform, transform, this.position);
+	mat4.rotateZ(transform, transform, this.rotation[2]*Math.PI/180);
+	mat4.rotateX(transform, transform, this.rotation[0]*Math.PI/180);
+	mat4.rotateY(transform, transform, this.rotation[1]*Math.PI/180);
 
 	var x0 = -0.5 * this.def.width;
 	var x1 = x0 + this.def.width;
-	console.log(this.def.type);
-	console.log(Object2DDefCache.Type.GROUND);
 	var y0 = this.def.type == Object2DDefCache.Type.GROUND ? -0.5 * this.def.height : 0.0;
 	var y1 = y0 + this.def.height;
+
 	vertices = [
 		x0, y0, 0,
 		x0, y1, 0,
@@ -56,9 +53,19 @@ Object2D.prototype.setDefinition = function(def)
 		x1, y1, 0,
 		x1, y0, 0
 	];
+	transformed_vertices = [];
+	for (var i = 0; i < vertices.length; i += 3)
+	{
+		v = vec3.fromValues(vertices[i], vertices[i+1], vertices[i+2]);
+		vec3.transformMat4(v, v, transform);
+		transformed_vertices.push(v[0]);
+		transformed_vertices.push(v[1]);
+		transformed_vertices.push(v[2]);
+	}
 	this.vertex_buffer = gl.createBuffer();
 	gl.bindBuffer(gl.ARRAY_BUFFER, this.vertex_buffer);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(transformed_vertices),
+		gl.STATIC_DRAW);
 	texture_coords = [
 		this.def.u_start, this.def.v_start,
 		this.def.u_start, this.def.v_end,
@@ -79,10 +86,6 @@ Object2D.prototype.draw = function()
 		// Definition still not loaded
 		return;
 
-	model_view_matrix.push();
-	model_view_matrix.multiply(this.matrix);
-	model_view_matrix.setUniform();
-
 	gl.uniform1f(shaders.program.alpha_low, this.def.alpha_test);
 
 	gl.bindBuffer(gl.ARRAY_BUFFER, this.vertex_buffer);
@@ -95,6 +98,4 @@ Object2D.prototype.draw = function()
 		gl.FLOAT, false, 0, 0);
 
 	gl.drawArrays(gl.TRIANGLES, 0, 6);
-
-	model_view_matrix.pop();
 }
