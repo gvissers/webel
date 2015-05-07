88aa8f242e49e075504324633f8885ff5c0036dc
Gé Vissers <ge@octonion.nl>
2015-05-07 15:34:21 +0200

    Speed up 2d object drawing a bit further by using a single GL buffer

--- Object2DMap.js
+++ Object2DMap.js
@@ -5,6 +5,25 @@ function Object2DMap()
 {
 	/// 2D objects, collected per definition
 	this.objects_for_def = {};
+	/// Total number of objects in this collection
+	this.nr_objects = 0;
+
+	/// Total number of objects for which definitions were loaded
+	this.nr_objects_loaded = 0;
+
+	/// Vertices for all objects in the file
+	this.vertices = null;
+	/// Texture coordinates for all objects in the file
+	this.texture_coords = null;
+	/// Vertex indices for all objects in the file
+	this.indices = null;
+
+	/// WebGL buffer for the object vertices
+	this.vertex_buffer = gl.createBuffer();
+	/// WebGL buffer for the texture coordinates
+	this.texture_coord_buffer = gl.createBuffer();
+	/// WebGL buffer for the element indices
+	this.index_buffer = gl.createBuffer();
 }
 
 /// Add the 2d object @a obj to this map
@@ -16,13 +35,12 @@ Object2DMap.prototype.add = function(obj)
 		this.objects_for_def[fname] = {
 			def: null,
 			objects: [],
-			vertex_buffer: null,
-			texture_coord_buffer: null,
-			index_buffer: null,
-			count: 0
+			count: 0,
+			offset: 0
 		};
 	}
 	this.objects_for_def[fname].objects.push(obj);
+	this.nr_objects++;
 }
 
 /**
@@ -30,6 +48,11 @@ Object2DMap.prototype.add = function(obj)
  */
 Object2DMap.prototype.setAllDefinitions = function()
 {
+	this.vertices = new Float32Array(4 * 3 * this.nr_objects);
+	this.texture_coords = new Float32Array(4 * 2 * this.nr_objects);
+	this.indices = new Uint16Array(2 * 3 * this.nr_objects);
+	this.nr_objects_loaded = 0;
+
 	var this_obj = this;
 	for (var fname in this.objects_for_def)
 	{
@@ -52,42 +75,49 @@ Object2DMap.prototype.setAllDefinitions = function()
  */
 Object2DMap.prototype.setDefinition = function(fname, def)
 {
+	var sq_idx = [0, 1, 2,   0, 2, 3];
 	var od = this.objects_for_def[fname];
+	var objs = od.objects;
 
 	od.def = def;
+	od.offset = 2 * 3 * 2 * this.nr_objects_loaded;
+	od.count = 2 * 3 * objs.length;
 
-	var vertices = [];
-	var texture_coords = [];
-	var indices = [];
-	var objs = od.objects;
 	for (var i = 0; i < objs.length; ++i)
 	{
-		var idx_off = vertices.length/3;
-
 		objs[i].setDefinition(def);
-		vertices = vertices.concat(objs[i].vertices);
-		texture_coords = texture_coords.concat(objs[i].texture_coords);
-		indices = indices.concat([
-			idx_off+0, idx_off+1, idx_off+2,
-			idx_off+0, idx_off+2, idx_off+3
-		]);
+
+		for (var j = 0; j < 4*3; ++j)
+			this.vertices[4*3*this.nr_objects_loaded+j] = objs[i].vertices[j];
+		for (var j = 0; j < 4*2; ++j)
+			this.texture_coords[4*2*this.nr_objects_loaded+j] = objs[i].texture_coords[j];
+		for (var j = 0; j < 2*3; ++j)
+			this.indices[2*3*this.nr_objects_loaded+j] = 4*this.nr_objects_loaded + sq_idx[j];
+
+		this.nr_objects_loaded++;
 	}
 
-	od.vertex_buffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, od.vertex_buffer);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-	od.texture_coord_buffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, od.texture_coord_buffer);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(texture_coords), gl.STATIC_DRAW);
-	od.index_buffer = gl.createBuffer();
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, od.index_buffer);
-	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
-	od.count = indices.length;
+	gl.bindBuffer(gl.ARRAY_BUFFER, this.vertex_buffer);
+	gl.bufferData(gl.ARRAY_BUFFER, this.vertices, gl.STATIC_DRAW);
+	gl.bindBuffer(gl.ARRAY_BUFFER, this.texture_coord_buffer);
+	gl.bufferData(gl.ARRAY_BUFFER, this.texture_coords, gl.STATIC_DRAW);
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.index_buffer);
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, this.indices, gl.STATIC_DRAW);
 }
 
 /// Draw the collection of 2D objects
 Object2DMap.prototype.draw = function()
 {
+	gl.bindBuffer(gl.ARRAY_BUFFER, this.vertex_buffer);
+	gl.vertexAttribPointer(shaders.program.vertexPositionAttribute, 3,
+		gl.FLOAT, false, 0, 0);
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, this.texture_coord_buffer);
+	gl.vertexAttribPointer(shaders.program.textureCoordAttribute, 2,
+		gl.FLOAT, false, 0, 0);
+
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.index_buffer);
+
 	for (var fname in this.objects_for_def)
 	{
 		var od = this.objects_for_def[fname];
@@ -97,18 +127,7 @@ Object2DMap.prototype.draw = function()
 
 		gl.uniform1f(shaders.program.alpha_low, od.def.alpha_test);
 		gl.bindTexture(gl.TEXTURE_2D, od.def.texture);
-
-		gl.bindBuffer(gl.ARRAY_BUFFER, od.vertex_buffer);
-		gl.vertexAttribPointer(shaders.program.vertexPositionAttribute, 3,
-			gl.FLOAT, false, 0, 0);
-
-		gl.bindBuffer(gl.ARRAY_BUFFER, od.texture_coord_buffer);
-		gl.vertexAttribPointer(shaders.program.textureCoordAttribute, 2,
-			gl.FLOAT, false, 0, 0);
-
-		gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, od.index_buffer);
-
-		gl.drawElements(gl.TRIANGLES, od.count, gl.UNSIGNED_SHORT, 0);
+		gl.drawElements(gl.TRIANGLES, od.count, gl.UNSIGNED_SHORT, od.offset);
 	}
 
 	// Reset alpha limit
