9dbbabf11020f26b21ccf7d44e01c1a5ac98f790
Gé Vissers <ge@octonion.nl>
2015-05-21 09:47:24 +0200

    Preload all particle system definitions at startup

--- ParticleSystemDefCache.js
+++ ParticleSystemDefCache.js
@@ -7,6 +7,20 @@ function ParticleSystemDefCache ()
 {
 	/// The cache itself, mapping file name to definition
 	this._cache = {};
+	/// Whether all currently known particle system definitions have been loaded yet
+	this._preloaded = false;
+	/// callbacks waiting to be executed
+	this._callbacks = [];
+
+	// preload all particle systems
+	var this_obj = this;
+	$.ajax("__all__.part", {
+		dataType: "json",
+		error: function() {
+			logError("Failed to initialize particle system cache");
+		},
+		success: function(defs) { this_obj.fill(defs); }
+	});
 };
 
 /// Enumeration for the various types of particle systems
@@ -17,6 +31,30 @@ ParticleSystemDefCache.Type = {
 	BURST: 3,
 	FIRE: 4,
 	FOUNTAIN: 5
+};
+
+/// Fill the cache with objects from the server
+ParticleSystemDefCache.prototype.fill = function(defs)
+{
+	if ("error" in defs)
+	{
+		logError(defs.error);
+	}
+	else
+	{
+		for (var fname in defs)
+			this._cache[fname] = new ParticleSystemDef(defs[fname]);
+		this._preloaded = true;
+
+		// Now try to get particle systems requested before preload completed
+		for (var i = 0; i < this._callbacks.length; ++i)
+		{
+			var cb = this._callbacks[i];
+			this.get(cb.fname, cb.callback);
+		}
+		this._callbacks = [];
+	}
+
 };
 
 /**
@@ -32,24 +70,33 @@ ParticleSystemDefCache.prototype.get = function(fname, callback)
 	var cache = this._cache;
 	if (!(fname in cache))
 	{
-		$.ajax(fname, {
-			dataType: "json",
-			error: function() {
-				logError("Failed to get particle system definition " + fname);
-			},
-			success: function(def) {
-				if ("error" in def)
-				{
-					logError(def.error)
+		if (!this._preloaded)
+		{
+			// we're still waiting for the preloaded data
+			if (callback)
+				this._callbacks.push({fname: fname, callback: callback});
+		}
+		else
+		{
+			$.ajax(fname, {
+				dataType: "json",
+				error: function() {
+					logError("Failed to get particle system definition " + fname);
+				},
+				success: function(def) {
+					if ("error" in def)
+					{
+						logError(def.error)
+					}
+					else
+					{
+						cache[fname] = new ParticleSystemDef(def);
+						if (callback)
+							callback(cache[fname]);
+					}
 				}
-				else
-				{
-					cache[fname] = new ParticleSystemDef(def);
-					if (callback)
-						callback(cache[fname]);
-				}
-			}
-		});
+			});
+		}
 		return null;
 	}
 	else
--- getpart.php
+++ getpart.php
@@ -1,5 +1,64 @@
 <?php
 
+function getpart($fname)
+{
+	$data = file_get_contents($fname);
+	$data = str_replace(',', ' ', $data);
+
+	$fields = sscanf($data, '%d %u %x %x %u %d %d %f %u %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f%n');
+error_log(print_r($fields,1));
+	if (count($fields) != 45)
+		throw new Exception("Unable to parse particle system definition in file {$fname}");
+
+	$res = array(
+		'version' => $fields[0],
+		'type' => $fields[1],
+		'blend' => array('source' => $fields[2], 'dest' => $fields[3]),
+		'count' => $fields[4],
+		'time_to_live' => $fields[5] < 0 ? -1 : 18*4*$fields[5],
+		'texture_fname' => "textures/particle{$fields[6]}.dds",
+		'size' => $fields[7],
+		'random_function' => $fields[8],
+		'min_position' => array($fields[9], $fields[10], $fields[11]),
+		'max_position' => array($fields[12], $fields[13], $fields[14]),
+		'radius_squared' => $fields[15],
+		'min_velocity' => array($fields[16], $fields[17], $fields[18]),
+		'max_velocity' => array($fields[19], $fields[20], $fields[21]),
+		'min_color' => array($fields[22], $fields[23], $fields[24], $fields[25]),
+		'max_color' => array($fields[26], $fields[27], $fields[28], $fields[29]),
+		'min_acceleration' => array($fields[30], $fields[31], $fields[32]),
+		'max_acceleration' => array($fields[33], $fields[34], $fields[35]),
+		'min_color_diff' => array($fields[36], $fields[37], $fields[38], $fields[39]),
+		'max_color_diff' => array($fields[40], $fields[41], $fields[42], $fields[43]),
+		'use_light' => false,
+		'light_position' => null,
+		'light_color' => null,
+		'sound' => -1
+	);
+
+	$data = trim(substr($data, $fields[44]));
+	if ($data)
+	{
+		$fields = sscanf($data, '%d %f %f %f %f %f %f%n');
+		if (count($fields) == 8)
+		{
+			$res['use_light'] = !!$fields[0];
+			$res['light_position'] = array($fields[1], $fields[2], $fields[3]);
+			$res['light_color'] = array($fields[4], $fields[5], $fields[6]);
+
+			$data = trim(substr($data, $fields[7]));
+			if ($data)
+			{
+				$fields = sscanf($data, '%d');
+				if ($fields)
+					$res['sound'] = $fields[0];
+			}
+		}
+	}
+
+	return $res;
+}
+
 header('Content-Type: application/json');
 try
 {
@@ -16,17 +75,20 @@ try
 	$have_json = file_exists($fname_json);
 
 	$read_json = false;
-	if (!$have_part)
+	if ($basename !== '__all__')
 	{
-		if (!$have_json)
-			throw new Exception('File not found');
-		$read_json = true;
-	}
-	else if ($have_json)
-	{
-		$stat_part = stat($fname_part);
-		$stat_json = stat($fname_json);
-		$read_json = $stat_part['mtime'] < $stat_json['mtime'];
+		if (!$have_part)
+		{
+			if (!$have_json)
+				throw new Exception('File not found');
+			$read_json = true;
+		}
+		else if ($have_json)
+		{
+			$stat_part = stat($fname_part);
+			$stat_json = stat($fname_json);
+			$read_json = $stat_part['mtime'] < $stat_json['mtime'];
+		}
 	}
 
 	if ($read_json)
@@ -36,58 +98,26 @@ try
 	}
 	else
 	{
-		$data = file_get_contents($fname_part);
-		$data = str_replace(',', ' ', $data);
-
-		$fields = sscanf($data, '%d %u %x %x %u %d %d %f %u %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f%n');
-error_log(print_r($fields,1));
-		if (count($fields) != 45)
-			throw new Exception('Unable to parse particle system definition');
-
-		$res = array(
-			'version' => $fields[0],
-			'type' => $fields[1],
-			'blend' => array('source' => $fields[2], 'dest' => $fields[3]),
-			'count' => $fields[4],
-			'time_to_live' => $fields[5] < 0 ? -1 : 18*4*$fields[5],
-			'texture_fname' => "textures/particle{$fields[6]}.dds",
-			'size' => $fields[7],
-			'random_function' => $fields[8],
-			'min_position' => array($fields[9], $fields[10], $fields[11]),
-			'max_position' => array($fields[12], $fields[13], $fields[14]),
-			'radius_squared' => $fields[15],
-			'min_velocity' => array($fields[16], $fields[17], $fields[18]),
-			'max_velocity' => array($fields[19], $fields[20], $fields[21]),
-			'min_color' => array($fields[22], $fields[23], $fields[24], $fields[25]),
-			'max_color' => array($fields[26], $fields[27], $fields[28], $fields[29]),
-			'min_acceleration' => array($fields[30], $fields[31], $fields[32]),
-			'max_acceleration' => array($fields[33], $fields[34], $fields[35]),
-			'min_color_diff' => array($fields[36], $fields[37], $fields[38], $fields[39]),
-			'max_color_diff' => array($fields[40], $fields[41], $fields[42], $fields[43]),
-			'use_light' => false,
-			'light_position' => null,
-			'light_color' => null,
-			'sound' => -1
-		);
-
-		$data = trim(substr($data, $fields[44]));
-		if ($data)
+		if ($basename == '__all__')
 		{
-			$fields = sscanf($data, '%d %f %f %f %f %f %f%n');
-			if (count($fields) == 8)
+			$res = array();
+			$iter = new RecursiveIteratorIterator(
+						new RecursiveDirectoryIterator('.',
+							FilesystemIterator::KEY_AS_PATHNAME
+							| FilesystemIterator::SKIP_DOTS
+							| FilesystemIterator::FOLLOW_SYMLINKS));
+			foreach ($iter as $fname => $path_obj)
 			{
-				$res['use_light'] = !!$fields[0];
-				$res['light_position'] = array($fields[1], $fields[2], $fields[3]);
-				$res['light_color'] = array($fields[4], $fields[5], $fields[6]);
-
-				$data = trim(substr($data, $fields[7]));
-				if ($data)
+				if ($path_obj->isFile() && $path_obj->getExtension() == 'part')
 				{
-					$fields = sscanf($data, '%d');
-					if ($fields)
-						$res['sound'] = $fields[0];
+					$sfname = substr($fname, 2);
+					$res[$sfname] = getpart($sfname);
 				}
 			}
+		}
+		else
+		{
+			$res = getpart($fname_part);
 		}
 
 		$json_data = json_encode($res);
