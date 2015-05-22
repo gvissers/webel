a6009690c829aa25bbab28394e94b66679ceab28
Gé Vissers <ge@octonion.nl>
2015-05-22 09:59:14 +0200

    Don't allocate a new normal matrix on every frame

--- ModelViewMatrix.js
+++ ModelViewMatrix.js
@@ -5,7 +5,11 @@
  */
 function ModelViewMatrix()
 {
+	/// Call parent constructor
 	GLMatrix.call(this, "model_view");
+
+	/// Normal matrix (inverse of rotation part of this matrix)
+	this.normal_matrix = mat3.create();
 }
 
 /// ModelViewMatrix derives from GLMatrix
@@ -14,7 +18,6 @@ ModelViewMatrix.prototype = Object.create(GLMatrix.prototype);
 /// Return the normal matrix for this model view
 ModelViewMatrix.prototype.normal = function()
 {
-	var normalMatrix = mat3.create();
-	mat3.normalFromMat4(normalMatrix, this.matrix);
-	return normalMatrix;
+	mat3.normalFromMat4(this.normal_matrix, this.matrix);
+	return this.normal_matrix;
 };
