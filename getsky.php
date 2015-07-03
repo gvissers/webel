fff34be58d3a1fb33819a56971be955a9f269525
Gé Vissers <ge@octonion.nl>
2015-07-03 13:16:56 +0200

    Fix getsky script so that all map-specific settings are stored, and text content is properly parsed

--- getsky.php
+++ getsky.php
@@ -1,33 +1,54 @@
 <?php
 
+function typify($str)
+{
+	$vval = strtolower($str);
+	if ( ($fval = filter_var($vval, FILTER_VALIDATE_INT,
+		array('flags' => FILTER_FLAG_ALLOW_OCTAL|FILTER_FLAG_ALLOW_HEX))) !== false )
+	{
+		return $fval;
+	}
+	if ( ($fval = filter_var($vval, FILTER_VALIDATE_FLOAT)) !== false )
+	{
+		return $fval;
+	}
+	if ( ($fval = filter_var($vval, FILTER_VALIDATE_BOOLEAN,
+		array('flags' => FILTER_NULL_ON_FAILURE))) !== null )
+	{
+		return $fval;
+	}
+	return $str;
+}
+
 function skyboxXMLToArray($node)
 {
+	if (!$node->hasAttributes() && $node->childNodes->length == 1
+		&& $node->childNodes[0]->nodeType == XML_TEXT_NODE)
+	{
+		return typify($node->textContent);
+	}
+
 	$res = array();
 	foreach ($node->attributes as $name => $val)
 	{
-		$vval = strtolower($val->value);
-		if ( ($fval = filter_var($vval, FILTER_VALIDATE_BOOLEAN,
-			array('flags' => FILTER_NULL_ON_FAILURE))) !== null )
-		{
-			$vval = $fval;
-		}
-		else if ( ($fval = filter_var($vval, FILTER_VALIDATE_INT,
-			array('flags' => FILTER_FLAG_ALLOW_OCTAL|FILTER_FLAG_ALLOW_HEX))) !== false )
-		{
-			$vval = $fval;
-		}
-		else if ( ($fval = filter_var($vval, FILTER_VALIDATE_FLOAT)) !== false )
-		{
-			$vval = $fval;
-		}
-		$res[$name] = $vval;
+		$res[$name] = typify(strtolower($val->value));
 	}
 
 	foreach ($node->childNodes as $child)
 	{
 		if ($child->nodeType == XML_ELEMENT_NODE)
 		{
-			$res[$child->tagName] = skyboxXMLToArray($child);
+			$child_data = skyboxXMLToArray($child);
+			if ($child->tagName == 'map')
+			{
+				$name = $child_data['name'];
+				unset($child_data['name']);
+				$res[$name] = $child_data;
+			}
+			else
+			{
+				$res[$child->tagName] = $child_data;
+			}
 		}
 	}
 
